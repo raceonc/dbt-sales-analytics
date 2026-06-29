e8a276433078c0e81a9c200b05d95d3415c1c34e
tmx_zs <tmxpvks96@gmail.com>
2026-06-29 10:48:11 +1000

    setup staging model

---
+++ models/staging/stg_sales_actuals.sql
@@ -0,0 +1,24 @@
+with source as (
+    select * from {{ ref('raw_sales_actuals')}}
+),
+
+renamed as (
+    select
+        sop_number,
+        sop_line,
+        cast(transaction_date as date) as transaction_date,
+        customer_code,
+        product_code,
+        scenario_code,
+        quantity,
+        unit_price,
+        unit_cost,
+        (quantity * unit_price) as gross_sales,
+        (quantity * unit_cost) as standard_cost_of_goods_sold,
+        (quantity * unit_price) 
+            - (quantity * unit_cost) as gross_margin
+        from source
+)
+
+
+select * from renamed
